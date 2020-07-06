a412e66a2d0f4b47371179299af26876756cb3b3
common <oliverjesingh@gmail.com>
2020-07-06 18:12:43 +0530

    Updates

---
+++ precache-manifest.a5d4eee14c8ee8a02820dd1a029d5078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b29dbb71096bc10e00da21bcbc339a",
+    "url": "/Assignment/index.html"
+  },
+  {
+    "revision": "5727befce40ba0562e85",
+    "url": "/Assignment/static/css/main.f461335e.chunk.css"
+  },
+  {
+    "revision": "28fa43af5a8c4b1a35ee",
+    "url": "/Assignment/static/js/2.da283ad4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Assignment/static/js/2.da283ad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5727befce40ba0562e85",
+    "url": "/Assignment/static/js/main.6343f64e.chunk.js"
+  },
+  {
+    "revision": "5e9e7c2f62b35dfa3853",
+    "url": "/Assignment/static/js/runtime-main.b7e6dccb.js"
+  }
+]);
